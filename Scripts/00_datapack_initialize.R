4ef811d4e67861f58df137a0609bf1868f882ede
Aaron <achafetz@usaid.gov>
2017-10-17 11:49:26 -0400

    refine code & file paths

--- Scripts/00_datapack_initialize.R
+++ Scripts/00_datapack_initialize.R
@@ -1,20 +1,19 @@
 ##  Data Pack COP18 
 ##  A.Chafetz, USAID
 ##  Purpose: initialize packages, folder structure, and global file paths
-##  Adapted from T. Essam, USAID
-##  Updated: 10/11/17 
+##  Adapted from T. Essam, USAID [Stata]
+##  Updated: 10/17/17 
 ##  https://github.com/achafetz/DataPack
 
 ## DEPENDENT PACKAGES -------------------------------------------------------------------------
   #load libraries 
-    lib <- c("tidyverse", "stringr", "forcats")
-    lapply(lib, require, character.only = TRUE)
-  
+    pacman::p_load("tidyverse", "stringr", "forcats")
+
 ## FILE PATHS ---------------------------------------------------------------------------------
   #must be run each time R project is opened
   #Choose the project path location to where you want the project parent 
   #folder to go on your machine.
-    projectpath <- "C:/Users/achafetz/Documents/GitHub"
+    projectpath <- "~/GitHub"
     setwd(projectpath)
 
   # Run a macro to set up study folder
@@ -24,16 +23,18 @@
 
   #Run initially to set up folder structure
   #Choose your folders to createa and and stored as values
-    folderlist <- c("RawData", "StataOutput", "ExcelOutput", "DoFiles")
+    folderlist <- c("RawData", "TempOutput", "Output", "Scripts")
     for (f in folderlist){
       dir.create(file.path(projectpath, pfolder, f))
       assign(tolower(f), file.path(projectpath, pfolder, f,"/"))
     }
-    
+
   #additional folders outside of project folder (due to large file size)
-    fvdata <- "C:/Users/achafetz/Documents/ICPI/Data"
-    dpexcel <- "C:/Users/achafetz/Documents/DataPack/DataPulls"
-  
-  
+    fvdata <- "~/ICPI/Data"
+    dpexcel <- "~/DataPack/DataPulls"
+
+
 ## CLEAN UP STORED GLOBALS -------------------------------------------------------------------
   rm(lib, projectpath, pfolder, folderlist, f)
+    
+    
--- Scripts/99_datapack_functions.R
+++ Scripts/99_datapack_functions.R
@@ -1,4 +1,4 @@
-##  Data Pack COP18 
+##  Data Pack COP18
 ##  A.Chafetz, USAID
 ##  Purpose: define all functions used in the scipting process
 ##  https://github.com/achafetz/DataPack
@@ -9,24 +9,26 @@ initialize_project <- function(pfolder, projectpath = "~/GitHub") {
   ##  Purpose: initialize packages, folder structure, and global file paths
   ##  Adapted from T. Essam, USAID
   ##  Updated: 10/11/17
-  
+
   ## DEPENDENT PACKAGES -----
-    #load libraries 
+    #load libraries
       pacman::p_load("tidyverse", "stringr", "forcats")
-  
+
   ## FILE PATHS -----
     # Setup subfolder structure
       dir.create(file.path(projectpath, pfolder), showWarnings = FALSE)
       setwd(file.path(projectpath, pfolder))
     
-
     # Choose your folders to create and and stored as values
       folderlist <- c("RawData", "TempOutput", "Output", "Scripts")
       for (f in folderlist){
         dir.create(file.path(projectpath, pfolder, f),showWarnings = FALSE)
         assign(tolower(f), file.path(projectpath, pfolder, f,"/"))
       }
-  
+
+    #additional folders outside of project folder (due to large file size)
+      fvdata <- "~/ICPI/Data"
+      dpexcel <- "~/DataPack/DataPulls"
   ## CLEAN UP STORED GLOBALS -----
     rm(projectpath, pfolder, folderlist, f)
 }
