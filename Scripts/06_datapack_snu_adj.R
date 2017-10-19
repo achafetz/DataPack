92d16e9b0f706d66e163cb2c646ad7616b728f13
Aaron <achafetz@usaid.gov>
2017-10-18 20:34:49 -0400

    functions

--- Scripts/06_datapack_snu_adj.R
+++ Scripts/06_datapack_snu_adj.R
@@ -3,15 +3,14 @@
 ##   Aaron Chafetz
 ##   Purpose: remove/combine duplicate SNUs with different UIDs & cluster SNUs
 ##   Date: January 12, 2017
-##   Updated: 10/17/17
+##   Updated: 10/18/17
 
-## COMBINE/DELETE SNUS ##
 
-  ##List of PSNUs that have the same name but different UIDs
-  ## Duplicate list produced from following do file
-  ## https://github.com/achafetz/ICPI_Projects/blob/master/Other/dupSNUs.do
-  ## N. Bartlett identified whether to combine/delete each
-  
+##List of PSNUs that have the same name but different UIDs
+## Duplicate list produced from following do file
+## https://github.com/achafetz/ICPI_Projects/blob/master/Other/dupSNUs.do
+## N. Bartlett identified whether to combine/delete each
+
 #   | operatingunit | psnu                    | psnuuid     | COP17 action        | COP18 action   |
 #   |---------------|-------------------------|-------------|---------------------|----------------|
 #   | Burma         | Dagon Myothit (Seikkan) | Z6b0Advh1f8 | N/A                 | Combine        |
@@ -45,49 +44,46 @@
 #   | India         | Chandigarh              | rdZgJxh6GA6 | N/A                 | No FY17 data   |
 #   | India         | Chandigarh              | eknq1Uf5JK6 | N/A                 | No FY17 data   |
 
+
+## COMBINE/DELETE SNUS -----------------------------------------------------------------------------
+
+cleanup_snus <- function(df) {
   
   #table of dup PSNUs(psnuuid) & their replacments (psnuuid_adj)
-    df_adj <- tribble(
-            ~psnuuid,	    ~psnuuid_adj,
-       "Z6b0Advh1f8",    "qPyHEwO7X6D",
-       "EzsXkY9WARj",    "URj9zYi533e",
-       "KN2TmcAVqzi",    "bDoKaxNx2Xb",
-       "kxsmKGMZ5QF",    "mVuyipSx9aU",
-       "FjiNyXde6Ae",    "xmRjV3Gx1H6",
-       "J4yYjIqL7mG",    "oygNEfySnMl"
-    )
-      
-  #replace duplicate UIDs so only one per PSNU
-    df_curr <- df_curr %>%
-      left_join(df_adj, by = "psnuuid") %>%
-      mutate(psnuuid = ifelse(is.na(psnuuid_adj), psnuuid, psnuuid_adj)) %>%
-      select(-psnuuid_adj) %>%
-    
-  #replace PNSU ek Ikere-Ekiti with ek Ikere
-      mutate(psnu = ifelse(psnuuid=="KT3e5pmPdfB","ek Ikere", psnu)) %>%
-    
-  #remove all duplicates/blank PSNUs
-      filter(!psnuuid %in% c("dOQ8r7iwZvS", "HHDEeZbVEaw", "IxeWi5YG9lE", "KT3e5pmPdfB", "h61xiVptz4A", 
-                             "lC1wneS1GR5", "D47MUIzTapM", "RVzTHBO9fgR")) %>%
+  df_adj <- tribble(
+    ~psnuuid,	    ~psnuuid_adj,
+    "Z6b0Advh1f8",    "qPyHEwO7X6D",
+    "EzsXkY9WARj",    "URj9zYi533e",
+    "KN2TmcAVqzi",    "bDoKaxNx2Xb",
+    "kxsmKGMZ5QF",    "mVuyipSx9aU",
+    "FjiNyXde6Ae",    "xmRjV3Gx1H6",
+    "J4yYjIqL7mG",    "oygNEfySnMl"
+  )
   
-  #add country name to regional programs
-     mutate(psnu = ifelse((operatingunit %in% 
+  #replace duplicate UIDs so only one per PSNU
+  df <- df %>%
+    left_join(df_adj, by = "psnuuid") %>%
+    mutate(psnuuid = ifelse(is.na(psnuuid_adj), psnuuid, psnuuid_adj)) %>%
+    select(-psnuuid_adj) %>%
+    
+    #replace PNSU ek Ikere-Ekiti with ek Ikere
+    mutate(psnu = ifelse(psnuuid=="KT3e5pmPdfB","ek Ikere", psnu)) %>%
+    
+    #remove all duplicates/blank PSNUs
+    filter(!psnuuid %in% c("dOQ8r7iwZvS", "HHDEeZbVEaw", "IxeWi5YG9lE", "KT3e5pmPdfB", "h61xiVptz4A", 
+                           "lC1wneS1GR5", "D47MUIzTapM", "RVzTHBO9fgR")) %>%
+    
+    #add country name to regional programs
+    mutate(psnu = ifelse((operatingunit %in% 
                             c("Asia Regional Program", "Caribbean Region", "Central America Region", "Central Asia Region")), 
-                         paste(snu1, psnu, sep = "/"), psnu))
-    rm(df_adj)
-
+                         paste(snu1, psnu, sep = "/"), psnu)) %>%
     
+    ## REMOVE SNUs ##
+    #S.Ally (1/17/17) - no Sustained - Commodities districts 
+    filter(!psnuuid %in% c("O1kvkveo6Kt", "hbnRmYRVabV", "N7L1LQMsQKd", "nlS6OMUb6s3")) %>%
     
-## REMOVE SNUs ##
-  #S.Ally (1/17/17) - no Sustained - Commodities districts 
-      df_curr <- df_curr %>%
-        filter(!psnuuid %in% c("O1kvkveo6Kt", "hbnRmYRVabV", "N7L1LQMsQKd", "nlS6OMUb6s3"))
-
-
-
-## SNU NAMING ISSUES ##
-  # M. Melchior (1/21/17) - txt import issue with French names 
-   df_curr <- df_curr %>%
+    ## SNU NAMING ISSUES ##
+    # M. Melchior (1/21/17) - txt import issue with French names 
     mutate( psnu = ifelse(psnuuid == "JVXPyu8T2fO", "Cap-Haïtien", psnu), 
             psnu = ifelse(psnuuid == "XXuTiMjae3r", "Anse à Veau", psnu),
             psnu = ifelse(psnuuid == "prA0IseYHWD", "Fort Liberté", psnu),
@@ -103,33 +99,36 @@
             psnu = ifelse(psnuuid == "R2NsUDhdF8x", "Saint-Raphaël", psnu),
             psnu = ifelse(psnuuid == "mLFKTGjlEg1", "Chardonniàres", psnu),
             psnu = ifelse((psnuuid %in% c("ONUWhpgEbVk", "RVzTHBO9fgR")), "Vallières", psnu)
-            ) 
-   
-   
-## Cluster SNUs ##
-  # clusters submitted by SI advisors - https://github.com/achafetz/ICPI/tree/master/DataPack/RawData
-  # only for psnu and psnu x im datasets, not site (orgunituid should not exist in PSNU or PSNU IM dataset) 
+    ) 
+  
+}
 
+
+
+
+
+## Cluster SNUs --------------------------------------------------------------------------------------------
+# clusters submitted by SI advisors - https://github.com/achafetz/ICPI/tree/master/DataPack/RawData
+# only for psnu and psnu x im datasets, not site (orgunituid should not exist in PSNU or PSNU IM dataset) 
+
+cluster_snus <- function(df){
   # import cluster dataset
-    df_cluster  <- read_csv(file.path(rawdata, "COP17Clusters.csv", sep=""))
-    
+  df_cluster  <- read_csv(file.path(rawdata, "COP17Clusters.csv", sep=""))
+  
   # remove duplicate data/headers
-    df_cluster <- select(df_cluster, -operatingunit, -psnu, -fy17snuprioritization, -cluster_set:-cluster_date)
-    
+  df_cluster <- select(df_cluster, -operatingunit, -psnu, -fy17snuprioritization, -cluster_set:-cluster_date)
+  
   # merge clusters onto factview
-    df_curr <- left_join(df_curr, df_cluster, by = "psnuuid")
-    rm(df_cluster)
-      
+  df <- left_join(df, df_cluster, by = "psnuuid")
+  
   # replace with cluster info
-    df_curr <- df_curr %>%
-      mutate(
-        psnu = ifelse(is.na(cluster_psnu), psnu, cluster_psnu),
-        snu1 = ifelse(is.na(cluster_snu1), snu1, cluster_snu1),
-        psnuuid = ifelse(is.na(cluster_psnuuid), psnuuid, cluster_psnuuid),
-        fy17snuprioritization = ifelse(is.na(cluster_fy17snuprioritization), fy17snuprioritization, cluster_fy17snuprioritization)
-        ) %>%
-      select(-cluster_psnu:-cluster_fy17snuprioritization)
-    
-    
-    
-    
+  df <- df %>%
+    mutate(
+      psnu = ifelse(is.na(cluster_psnu), psnu, cluster_psnu),
+      snu1 = ifelse(is.na(cluster_snu1), snu1, cluster_snu1),
+      psnuuid = ifelse(is.na(cluster_psnuuid), psnuuid, cluster_psnuuid),
+      fy17snuprioritization = ifelse(is.na(cluster_fy17snuprioritization), fy17snuprioritization, cluster_fy17snuprioritization)
+    ) %>%
+    select(-cluster_psnu:-cluster_fy17snuprioritization)
+  
+} 
