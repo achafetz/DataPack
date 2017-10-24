cfce859057abcf9aee85dec2cd4a65c3cb9b808f
Aaron <achafetz@usaid.gov>
2017-10-24 16:44:13 -0400

    update

--- Scripts/02_datapack_output_keyind.R
+++ Scripts/02_datapack_output_keyind.R
@@ -40,9 +40,9 @@
     #add "indicators" for spaces
         bind_rows(
           tribble(~header, "a_sp1", "a_sp2", "HTS_TST_msp1", "HTS_TST_msp2", "HTS_TST_msp3", 
-                             "HTS_TST_POS_sp1", "HTS_TST_POS_sp2", "HTS_TST_POS_sp3", "PMTCT_ART_sp1", 
-                             "PMTCT_ART_sp2", "PMTCT_ART_sp3", "TB_ART_sp1", "TB_ART_sp2", "TB_ART_sp3", 
-                             "TX_CURR_sp1", "TX_CURR_sp2", "TX_CURR_sp3", "TX_NEW_sp1", "TX_NEW_sp2", "TX_NEW_sp3")
+                           "HTS_TST_POS_sp1", "HTS_TST_POS_sp2", "HTS_TST_POS_sp3", "PMTCT_ART_sp1", 
+                           "PMTCT_ART_sp2", "PMTCT_ART_sp3", "TB_ART_sp1", "TB_ART_sp2", "TB_ART_sp3", 
+                           "TX_CURR_sp1", "TX_CURR_sp2", "TX_CURR_sp3", "TX_NEW_sp1", "TX_NEW_sp2", "TX_NEW_sp3")
           ) %>%
         
     #reshape with variables as column headers 
@@ -52,10 +52,12 @@
         select(operatingunit, psnuuid, priority_snu, snulist, a_sp1:VMMC_CIRC_fy2018_targets)
       
 ## EXPORT -----------------------------------------------------------------------------------------------------  
-    
-    write_csv(df_keyindtbl, file.path(output, paste("Global_KeyTrends.csv", sep="")))
-      rm(df_keyindtbl)
-      
+   
+    #export 
+      write_csv(df_keyindtbl, file.path(output, paste("Global_KeyTrends.csv", sep="")))
+        rm(df_keyindtbl)
+        file.remove(file.path(tempoutput, "append_temp.Rdata"))
+        
       
       
       
