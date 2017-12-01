ee5d0cd250dbfd40995185e4fc171ec3a41ede12
Aaron <achafetz@usaid.gov>
2017-11-30 21:36:45 -0500

    update variable creation

--- Scripts/04_datapack_im_targeting.R
+++ Scripts/04_datapack_im_targeting.R
@@ -167,6 +167,7 @@
           kp_mat = ifelse((indicator=="KP_MAT" & standardizeddisaggregate=="Total Numerator" & numeratordenom=="N"), fy2017apr, 0), 
           gend_gbv = ifelse((indicator=="GEND_GBV" & standardizeddisaggregate=="Total Numerator" & numeratordenom=="N"), fy2017apr, 0), 
           prep_new = ifelse((indicator=="PrEP_NEW" & standardizeddisaggregate=="Total Numerator" & numeratordenom=="N"), fy2017apr, 0))
+    
       
       
 ## CLEAN UP -------------------------------------------------------------------------------------------------
