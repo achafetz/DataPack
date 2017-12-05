791b8257fb787bbe81ba5ce67594ecca6047e55c
Aaron <achafetz@usaid.gov>
2017-12-05 17:06:29 -0500

    modify df name & remove testing portion

--- Scripts/11_datapack_disagg_targeting.R
+++ Scripts/11_datapack_disagg_targeting.R
@@ -3,7 +3,7 @@
 ##   Purpose: generate disagg distribution for targeting
 ##   Adopted from COP17 Stata code
 ##   Date: Oct 26, 2017
-##   Updated: 11/10/17 
+##   Updated: 12/05/17 
 
 ## DEPENDENCIES
 # run 00_datapack_initialize.R
@@ -13,37 +13,28 @@
 ## FILTER DATA ------------------------------------------------------------------------------------------------------    
 
   # import
-    df_imalloc  <- read_tsv(file.path(fvdata, paste("ICPI_FactView_PSNU_", datestamp, ".txt", sep=""))) %>% 
+    df_psnualloc  <- read_tsv(file.path(fvdata, paste("ICPI_FactView_PSNU_", datestamp, ".txt", sep=""))) %>% 
       rename_all(tolower)
   
   #cleanup PSNUs (dups & clusters)
     source(file.path(scripts, "92_datapack_snu_adj.R"))
-    cleanup_snus(df_imalloc)
-    cluster_snus(df_imalloc)
+    cleanup_snus(df_psnualloc)
+    cluster_snus(df_psnualloc)
     
-  ## REMOVE BELOW  -------------------------------------------------------------------------------------------------
-  
-  ## FOR TESTING ONLY ## REMOVE after FY17 APR becomes available ##
-
-    source(file.path(scripts, "93_datapack_testingdata.R"))
-    testing_dummydata(df_imalloc)
-  
-  #  ^^^^^^ REMOVE ABOVE ^^^^^^
   
 ## SUBSET DATA OF INTEREST  ---------------------------------------------------------------------------------------  
 
-  df_imalloc <- df_imalloc %>% 
+  df_psnualloc <- df_psnualloc %>% 
     
     # limit to indicators with targets for COP18 (no MCAD disaggs)
-      filter(indicator %in% c("GEND_GBV", "HTS_SELF", "HTS_TST_POS", "HTS_TST_NEG", "KP_MAT", 
-                               "KP_PREV", "OVC_HIVSTAT", "OVC_SERV", "PMTCT_ART", 
-                               "PMTCT_EID", "PMTCT_STAT", "PP_PREV", "PREP_NEW", 
-                               "TB_ART", "TB_PREV", "TB_STAT", "TX_CURR", "TX_NEW", 
-                               "TX_PVLS", "TX_RET", "TX_TB", "VMMC_CIRC"),
+      filter(indicator %in% c("GEND_GBV", "OVC_SERV", "PMTCT_EID", "PMTCT_STAT", 
+                              "PP_PREV", "PrEP_NEW", "TB_ART", "TB_PREV", "TB_STAT", 
+                              "TX_CURR", "TX_NEW", "TX_PVLS", "TX_RET", "TX_TB", 
+                              "VMMC_CIRC"),
              ismcad == "N",
              !is.na(fy2017apr))
     # limit to just key variables
-      select(operatingunit, psnuuid, psnu, fy18snuprioritization, indicator:indicatortype, standardizeddisaggregate, age:otherdisaggregate, modality, fy2017apr) %>% 
+      select(operatingunit, psnuuid, psnu, fy17snuprioritization, indicator:indicatortype, standardizeddisaggregate, age:otherdisaggregate, modality, fy2017apr) %>% 
     
     # aggregate to psnu x disagg [type] level to have one line per obs
       group_by_if(is.character) %>%
@@ -53,7 +44,7 @@
 
 ## SUBSET DATA OF INTEREST  ---------------------------------------------------------------------------------------  
     
-  df_imalloc <- df_imalloc %>% 
+  df_psnualloc <- df_psnualloc %>% 
     
     # adjust disagg name so can create shares for Peds + Adults for TX_CURR, TX_NEW, and HTS_TST
       mutate(disagg = ifelse(indicator %in% c("TX_CURR", "TX_NEW", "HTS_TST_POS", "HTS_TST_NEG") & age %in% c("<01", "01-09", "10-14"), "Peds Age/Sex",
@@ -66,7 +57,7 @@
 
 ## DISAGG ALLOCATION  ---------------------------------------------------------------------------------------  
     
-  df_imalloc <- df_imalloc %>% 
+  df_psnualloc <- df_psnualloc %>% 
     # create a disagg group total as the denominator for the allocation share
       group_by(operatingunit, psnuuid, psnu, fy18snuprioritization, indicator, numeratordenom, indicatortype, disagg) %>% 
       mutate(total = sum(fy2017apr)) %>% 
