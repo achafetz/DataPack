058cb991416537e19d62a173ee957d9da943b26d
Aaron <achafetz@usaid.gov>
2017-12-06 19:59:11 -0500

    rework approach to dataset creation; still draft

--- Scripts/11_datapack_disagg_targeting.R
+++ Scripts/11_datapack_disagg_targeting.R
@@ -3,7 +3,7 @@
 ##   Purpose: generate disagg distribution for targeting
 ##   Adopted from COP17 Stata code
 ##   Date: Oct 26, 2017
-##   Updated: 12/05/17 
+##   Updated: 12/06/17 
 
 ## DEPENDENCIES
 # run 00_datapack_initialize.R
@@ -20,6 +20,7 @@
     source(file.path(scripts, "92_datapack_snu_adj.R"))
     cleanup_snus(df_psnualloc)
     cluster_snus(df_psnualloc)
+      rm(cleanup_snus, cluster_snus)
     
   
 ## SUBSET DATA OF INTEREST  ---------------------------------------------------------------------------------------  
@@ -32,9 +33,10 @@
                               "TX_CURR", "TX_NEW", "TX_PVLS", "TX_RET", "TX_TB", 
                               "VMMC_CIRC"),
              ismcad == "N",
-             !is.na(fy2017apr))
+             !is.na(fy2017apr), 
+             fy2017apr!=0) %>% 
     # limit to just key variables
-      select(operatingunit, psnuuid, psnu, fy17snuprioritization, indicator:indicatortype, standardizeddisaggregate, age:otherdisaggregate, modality, fy2017apr) %>% 
+      select(operatingunit, psnuuid, psnu, fy17snuprioritization, indicator:indicatortype, standardizeddisaggregate, age:modality, fy2017apr) %>% 
     
     # aggregate to psnu x disagg [type] level to have one line per obs
       group_by_if(is.character) %>%
@@ -42,19 +44,25 @@
       ungroup() 
 
 
-## SUBSET DATA OF INTEREST  ---------------------------------------------------------------------------------------  
+## GENERATE VARIABLES/COLUMNS -------------------------------------------------------------------------------------
+    # output formulas created in Data Pack template (POPsubset sheet)
+    # updated 12/6
     
   df_psnualloc <- df_psnualloc %>% 
-    
-    # adjust disagg name so can create shares for Peds + Adults for TX_CURR, TX_NEW, and HTS_TST
-      mutate(disagg = ifelse(indicator %in% c("TX_CURR", "TX_NEW", "HTS_TST_POS", "HTS_TST_NEG") & age %in% c("<01", "01-09", "10-14"), "Peds Age/Sex",
-                             ifelse(indicator %in% c("TX_CURR", "TX_NEW", "HTS_TST", "HTS_TST_POS", "HTS_TST_NEG") & grepl("AgeAbove", standardizeddisaggregate), "Adult Age/Sex",
-                                    standardizeddisaggregate)
-                             )) %>% 
-    # remove old disagg indicator
-      select(-standardizeddisaggregate)
-  
+      mutate(
+       
+      )
 
+      
+ ## RESHAPE  ---------------------------------------------------------------------------------------    
+  
+    test <- df_psnualloc %>%
+      #reshape to long form to then create aggregate group denominator for distro
+        select(-indicator:-fy2017apr) %>% 
+        gather(ind, val, -operatingunit:-fy17snuprioritization)
+      
+      test <- test %>%  filter(val!=0)
+      
 ## DISAGG ALLOCATION  ---------------------------------------------------------------------------------------  
     
   df_psnualloc <- df_psnualloc %>% 
@@ -66,5 +74,25 @@
     # create allocation share for each disagg type   
       mutate(share = round(fy2017apr/total, 3))
   
+      
+      
+      filter(!str_detect(otherdisaggregate, "Unknown Sex")) %>% 
+        mutate(grouping = ifelse(indicator == "OVC_SERV" & standardizeddisaggregate == "Age/Sex/Service", paste(standardizeddisaggregate, otherdisaggregate, sep = " - "), grouping),
+               grouping = ifelse(indicator == "OVC_SERV" & standardizeddisaggregate == "Age/Sex" & (age %in% c("<01", "01-04", "05-09","10-14", "15-17")), paste(standardizeddisaggregate, "<18", sep = " - "), grouping),
+               grouping = ifelse(indicator == "OVC_SERV" & standardizeddisaggregate == "Age/Sex" & (age %in% c("18-24", "25+")), paste(standardizeddisaggregate, "18+", sep = " - "), grouping),
+               grouping = ifelse(indicator == "PMTCT_STAT" & standardizeddisaggregate == "Age/KnownNewResult", paste(standardizeddisaggregate, otherdisaggregate, resultstatus, sep = " "), grouping),
+               grouping = ifelse(indicator == "TB_STAT" & standardizeddisaggregate == "Age/Sex/KnownNewPosNeg", paste(standardizeddisaggregate, otherdisaggregate, resultstatus, sep = " "), grouping),
+               grouping = ifelse((indicator %in% c("TX_CURR", "TX_NEW")) & (standardizeddisaggregate %in% c("AgeLessThanTen", "AgeAboveTen/Sex")) & (age %in% c("<01", "01-09", "10-14")), paste(standardizeddisaggregate, "<15", sep = " - "), grouping),
+               grouping = ifelse((indicator %in% c("TX_CURR", "TX_NEW")) & standardizeddisaggregate=="AgeAboveTen/Sex" & (age %in% c("15-19", "20-24", "50+")), paste(standardizeddisaggregate, "15+", sep = " - "), grouping),
+               grouping = ifelse(indicator == "TX_RET" & standardizeddisaggregate == "Aggregated Age/Sex" & age != "Unknown Age", paste(standardizeddisaggregate, age, sep = " - "),
+               grouping = ifelse(indicator == "VMMC_CIRC" & standardizeddisaggregate == "Age" & (age %in% c("15-19", "20-24", "25-29")), paste(standardizeddisaggregate, "Primary", sep = " - "), grouping),
+               grouping = ifelse(indicator == "VMMC_CIRC" & standardizeddisaggregate == "Age" & (age %in% c("[months] 00-02", "02 months - 09 years", "10-14", "50+")), paste(standardizeddisaggregate, "Other", sep = " - "), grouping))
+        )
 
-
+      
+      disaggs <- read_tsv(file.path(rawdata, "disagg_ind_grps.txt")) %>% 
+        filter(!is.na(standardizeddisaggregate))  %>% 
+        select(-dt_dataelementgrp, -dt_categoryoptioncombo)
+      
+      
+      test <- full_join(df_psnualloc, disaggs)
