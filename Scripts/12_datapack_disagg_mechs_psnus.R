926214cd4c033712a95b59e2f6b135da07b79472
Aaron <achafetz@usaid.gov>
2017-12-08 11:01:30 -0500

    bug fix

--- Scripts/12_datapack_disagg_mechs_psnus.R
+++ Scripts/12_datapack_disagg_mechs_psnus.R
@@ -8,7 +8,7 @@
 # run 00_datapack_initialize.R
 # ICPI Fact View PSNU_IM
 # 91_datapack_officialnames.R
-
+# 92_datapack_snu_adj.R
 
 ## SETUP  ----------------------------------------------------------------------------------------------
 
@@ -35,23 +35,26 @@
                               "PMTCT_ART", "PMTCT_EID", "PMTCT_STAT", "PP_PREV","PrEP_NEW", "TB_ART", "TB_PREV", 
                               "TB_STAT", "TX_CURR","TX_NEW", "TX_PVLS", "TX_RET", "TX_TB", "VMMC_CIRC")) %>% 
       filter(mechanismid>1) %>% #remove dedups 
-      distinct(operatingunit, psnuuid, psnu, fy17snuprioritization, mechanismid, implementingmechanismname, indicatortype) 
+      distinct(operatingunit, psnuuid, psnu, fy17snuprioritization, mechanismid, implementingmechanismname, indicatortype) %>% 
+      arrange(operatingunit, psnu,mechanismid, indicatortype)
     
 
 ## EXPORT -----------------------------------------------------------------------------------------------------  
 
-  write_csv(df_mechlist, file.path(output, "Global_MechList.csv"), na = "")
-    rm(df_mechlist)
+  write_csv(df_mechlist, file.path(output, "Global_DT_MechList.csv"), na = "")
     
 
 ## PSNU LIST  ----------------------------------------------------------------------------------------------
     
-    #unique list of mechanisms
-    df_mechlist <- df_factview %>%
-      filter(indicator %in% c("GEND_GBV", "HTS_SELF", "HTS_TST", "KP_PREV", "KP_MAT", "OVC_SERV", "OVC_HIVSTAT",  
-                              "PMTCT_ART", "PMTCT_EID", "PMTCT_STAT", "PP_PREV","PrEP_NEW", "TB_ART", "TB_PREV", 
-                              "TB_STAT", "TX_CURR","TX_NEW", "TX_PVLS", "TX_RET", "TX_TB", "VMMC_CIRC")) %>% 
-      filter(mechanismid>1) %>% #remove dedups 
-      distinct(operatingunit, psnuuid, psnu, fy17snuprioritization, mechanismid, implementingmechanismname, indicatortype) 
+    #unique list of PSNUs
+    df_psnulist <- df_mechlist %>%
+      distinct(operatingunit, psnuuid, psnu, fy17snuprioritization, indicatortype) %>% 
+      arrange(operatingunit, psnu, indicatortype)
+
     
+## EXPORT -----------------------------------------------------------------------------------------------------  
+    
+    write_csv(df_psnulist, file.path(output, "Global_DT_PSNUList.csv"), na = "")
+    
+    rm(df_factview, df_mechlist, df_psnulist)
     
