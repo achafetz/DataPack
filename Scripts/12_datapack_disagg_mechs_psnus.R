1d365450368cd7eeeb421726d8d82f00b226ee92
Aaron <achafetz@usaid.gov>
2018-01-26 10:16:32 -0500

    update for PNSU non cluster output for PLHIV

--- Scripts/12_datapack_disagg_mechs_psnus.R
+++ Scripts/12_datapack_disagg_mechs_psnus.R
@@ -2,26 +2,26 @@
 ##   A.Chafetz, USAID
 ##   Purpose: output unique mechanism list and PSNU list
 ##   Date: Dec 8, 2017
-##   Updated: 1/16/18
+##   Updated: 1/26/18
 
 ## DEPENDENCIES
 # run 00_datapack_initialize.R
-# ICPI Fact View PSNU_IM
+# ICPI Fact View PSNU_IM and PSNU
 # 91_datapack_officialnames.R
 # 92_datapack_snu_adj.R
 
 ## SETUP  ----------------------------------------------------------------------------------------------
 
   #import
-    df_factview  <- read_rds(file.path(fvdata, paste0("ICPI_FactView_PSNU_IM_", datestamp, ".RDS")))
+    df_mechlist  <- read_rds(file.path(fvdata, paste0("ICPI_FactView_PSNU_IM_", datestamp, ".RDS")))
     
   #cleanup PSNUs (dups & clusters)
     source(file.path(scripts, "91_datapack_officialnames.R"))
-    df_factview <- cleanup_mechs(df_factview, rawdata)
+      df_mechlist <- cleanup_mechs(df_mechlist, rawdata)
       
     source(file.path(scripts, "92_datapack_snu_adj.R"))
-    df_factview <- cluster_snus(df_factview)
-    df_factview <- cleanup_snus(df_factview)
+      df_mechlist <- cluster_snus(df_mechlist)
+      df_mechlist <- cleanup_snus(df_mechlist)
     
     rm(cleanup_mechs, cleanup_snus, cluster_snus)
   
@@ -29,7 +29,7 @@
 ## MECHANISM LIST  ----------------------------------------------------------------------------------------------
     
   #unique list of mechanisms
-    df_mechlist <- df_factview %>%
+    df_mechlist <- df_mechlist %>%
       filter(indicator %in% c("GEND_GBV", "HTS_SELF", "HTS_TST", "KP_PREV", "KP_MAT", "OVC_SERV", "OVC_HIVSTAT",  
                               "PMTCT_ART", "PMTCT_EID", "PMTCT_STAT", "PP_PREV","PrEP_NEW", "TB_ART", "TB_PREV", 
                               "TB_STAT", "TX_CURR","TX_NEW", "TX_PVLS", "TX_RET", "TX_TB", "VMMC_CIRC")) %>% 
@@ -39,24 +39,23 @@
       arrange(operatingunit, psnu, mechanismid, indicatortype) %>%
       mutate(psnu_type = paste(psnu, indicatortype, sep = " "))
     
-
-## EXPORT -----------------------------------------------------------------------------------------------------  
-
-  write_csv(df_mechlist, file.path(output, "Global_DT_MechList.csv"), na = "")
-    
+  # export
+    write_csv(df_mechlist, file.path(output, "Global_DT_MechList.csv"), na = "")
+      rm(df_mechlist)
 
 ## PSNU LIST  ----------------------------------------------------------------------------------------------
     
-    #unique list of PSNUs
-    df_psnulist <- df_mechlist %>%
-      distinct(operatingunit, psnu, psnuuid, currentsnuprioritization, indicatortype, psnu_type) %>% 
-      select(operatingunit, psnu, psnuuid, currentsnuprioritization, indicatortype, psnu_type) %>% 
-      arrange(operatingunit, psnu, indicatortype)
-
+  #import
+    df_psnulist  <- read_rds(file.path(fvdata, paste0("ICPI_FactView_PSNU_", datestamp, ".RDS")))
+  
+  #unique list of PSNUs (non clusters for PLHIV import)
+    df_psnulist <- df_psnulist %>%
+      distinct(operatingunit, psnu, psnuuid) %>% 
+      filter(!is.na(psnuuid)) %>% 
+      select(operatingunit, psnu, psnuuid) %>% 
+      arrange(operatingunit, psnu, psnuuid)
     
-## EXPORT -----------------------------------------------------------------------------------------------------  
-    
+  #export
     write_csv(df_psnulist, file.path(output, "Global_DT_PSNUList.csv"), na = "")
-    
-    rm(df_factview, df_mechlist, df_psnulist)
+      rm(df_psnulist)
     
